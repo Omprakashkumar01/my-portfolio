fbf4c6e67d7fd88a51b17b9761d74187c8327810
Omprakashkr1 <112559646+Omprakashkr1@users.noreply.github.com>
2024-10-29 11:47:16 +0530

    experience card

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,7 @@ import img from "./asset/me2.jpg";
 import TypingAnimation from "./TypingAnimation";
 export default function Body() {
   return (
+    <div className="new">
     <div className="body1">
       <div className="intro-container">
       <h1 className="intro">Hi! I'm <span className="name">OM PRAKASH KUMAR</span></h1>
@@ -10,6 +11,9 @@ export default function Body() {
       </div>
       
       <img className="img1"src={img} alt="img" />
+      
+    </div>
+    <h3 className="resume"><a href="https://drive.google.com/file/d/1-xwrEDhxKSA4LPfcfG5DJ8Ls61NNn960/view?usp=sharing">My resume <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M10 20A10 10 0 1 0 0 10a10 10 0 0 0 10 10zM8.711 4.3l5.7 5.766L8.7 15.711l-1.4-1.422 4.289-4.242-4.3-4.347z"/></svg></a></h3>
     </div>
   );
 }
